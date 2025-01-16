ca9ad98289ee1460dc1c0b47dbd75d3f26b3acec
sarisiza <sarita.iza@gmail.com>
2025-01-15 18:18:40 -0600

    Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("com.google.dagger.hilt.android") version "2.44.2" apply false
 }
